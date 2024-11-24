28bf4a0375b29d0586e15c92567e0668b35ed2c0
volto9 <volto9@yahoo.com>
2024-11-24 22:16:40 +0000

    tt

--- src/app/post/posts/page.js
+++ src/app/post/posts/page.js
@@ -1,26 +1,26 @@
-import Link from 'next/link';
+// import Link from 'next/link';
 
-export default async function WineListPage({ searchParams }) {
-  // console.log('searchParams', searchParams);
-  const response = await fetch('http://localhost:3000/post/1');
-  const posts = await response.json();
+// export default async function WineListPage({ searchParams }) {
 
-  if (searchParams.sort === 'desc') {
-    posts.reverse();
-  }
+//   const response = await fetch('http://localhost:3000/post/1');
+//   const posts = await response.json();
 
-  return (
-    <div>
-      <h2>Wine Post</h2>
-      <Link href="/post?sort=asc">Sort ascending</Link>
-      <Link href="/post?sort=desc">Sort descending</Link>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>
-            <a href={`/post/${post.id}`}>{post.username}</a>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+//   if (searchParams.sort === 'desc') {
+//     posts.reverse();
+//   }
+
+//   return (
+//     <div>
+//       <h2>Wine Post</h2>
+//       <Link href="/post?sort=asc">Sort ascending</Link>
+//       <Link href="/post?sort=desc">Sort descending</Link>
+//       <ul>
+//         {posts.map((post) => (
+//           <li key={post.id}>
+//             <a href={`/post/${post.id}`}>{post.username}</a>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
